e0031ee05a88945a8987b16fc4181fbb006d0c4a
choiseongjun <csj2702@naver.com>
2023-04-14 18:19:55 +0900

    .

--- src/components/nav/Navigation.tsx
+++ src/components/nav/Navigation.tsx
@@ -10,10 +10,10 @@ const Navigation = () => {
           <div>홈</div>
           <div>회사소개</div>
           <div>서비스 신청하기</div>
+          <div>후기</div>
+
           <div>자주하는 질문</div>
           <div>공지사항</div>
-
-          <div>후기</div>
         </div>
       </div>
     </div>
