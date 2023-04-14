f5109cc17ea5f7044b7e67c8820a949c596d0c1e
choiseongjun <csj2702@naver.com>
2023-04-14 18:11:32 +0900

    네비게이션 추가

---
+++ src/components/nav/Navigation.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import style from "./nav.module.css";
+
+const Navigation = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.box}>
+        <div>SB GLOBAL</div>
+        <div className={style.rightBox}>
+          <div>홈</div>
+          <div>회사소개</div>
+          <div>서비스 신청하기</div>
+          <div>자주하는 질문</div>
+          <div>공지사항</div>
+
+          <div>후기</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
