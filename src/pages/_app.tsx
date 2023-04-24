b4b579bd181e625f3335a0ffc9218908d51a9b3a
choiseongjun <csj2702@naver.com>
2023-04-24 13:56:18 +0900

    폰트변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,7 +51,12 @@ export default function App({
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
           crossOrigin="anonymous"
         />
-
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap"
+          rel="stylesheet"
+        ></link>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
