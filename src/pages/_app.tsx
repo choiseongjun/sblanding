e6a48f08785bf5a0ba5c4cace5584d7fd6c07583
choiseongjun <csj2702@naver.com>
2023-04-25 10:44:15 +0900

    slick css link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,6 +33,16 @@ export default function App({ Component, pageProps }: any) {
           href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
 
         <meta
           name="viewport"
