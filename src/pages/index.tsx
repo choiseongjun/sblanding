4320a19867ef0c9df27c092cc81153530e67ffe5
응띠쀼 <chldmsdud827@naver.com>
2023-04-28 12:03:29 +0900

    폰트변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -123,7 +123,7 @@ export default function Home({ title, description, url, keyword }: any) {
                       backgroundColor: "#000",
                       color: "yellow",
                       padding: 15,
-                      font: "GmarketSansMedium",
+                      fontFamily: "GmarketSansMedium",
                     }}
                   >
                     1:1 카톡 상담하기{" "}
