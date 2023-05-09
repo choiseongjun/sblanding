be506f4efa532de9746c8975534c620d5bd4c99e
응띠쀼 <chldmsdud827@naver.com>
2023-05-09 14:29:56 +0900

    상단바

---
+++ src/components/top/top.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./top.module.css";
+import { Link } from "react-scroll";
+
+const top = () => {
+    return (
+    <div className={style.container1}>
+      <div className={style.box1}>
+        <div>SB GLOBAL</div>
+        </div>
+    </div>
+  );
+};
+
+export default top;
