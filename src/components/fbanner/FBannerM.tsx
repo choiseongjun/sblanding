5faf644a4015b4a94e7ae5033ad6d57a14ae5ac6
응띠쀼 <chldmsdud827@naver.com>
2023-05-23 17:34:29 +0900

    모달

--- src/components/fbanner/FBannerM.tsx
+++ src/components/fbanner/FBannerM.tsx
@@ -3,6 +3,7 @@ import style from "./FBanner.module.css";
 import Form from "react-bootstrap/Form";
 import { FormCheck } from "react-bootstrap";
 import TelegramApi from "node-telegram-api";
+import { Modal } from "react-responsive-modal";
 
 const FBanner = () => {
   const [userName, setUserName] = useState("");
