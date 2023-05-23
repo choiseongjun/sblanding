7f73ead24ffca245ea3ed282a0517c2e9938b7e2
응띠쀼 <chldmsdud827@naver.com>
2023-05-23 09:20:29 +0900

    변경

--- src/components/stockInfo/QuntApply.tsx
+++ src/components/stockInfo/QuntApply.tsx
@@ -3,12 +3,10 @@ import style from "./StockInfo.module.css";
 
 const QuntApply = () => {
   return (
-      <div onClick={() => quantbox(window.open("https://quantbox.kr", "_blank"))} 
-        className={style.imgCenter}>
-        <img src="/static/images/quntapply.jpeg" />
-        
-        
+    <div className={style.imgCenter}>
+      <img src="/static/images/quntapply.jpeg" />
     </div>
   );
 };
 
+export default QuntApply;
