7b246c8c3ec9dea9380b7575aa783702676fdae6
응띠쀼 <chldmsdud827@naver.com>
2023-05-26 12:29:00 +0900

    가운데정렬

--- src/components/nav/Navigation.tsx
+++ src/components/nav/Navigation.tsx
@@ -5,9 +5,9 @@ import { Link } from "react-scroll";
 const Navigation = () => {
   return (
     <div className={style.container}>
-      <div className={style.box} style="display: flex; justify-content: center;">
+      <div className={style.box}  >
         < img
-          style={{ width : 100 , left: 60  }}
+          style={{ width : 100 , left: 60 , display: flex , justify-content: center }}
          src="/static/images/sblogo.png" />
         </div>
         {/* <div>SB GLOBAL</div> */}
